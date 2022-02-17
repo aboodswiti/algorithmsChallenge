009afb9075bdbeecc78eb3aa1a4bce00c5530ef2
aboodswiti <aboodprinc@gmailcom>
2022-02-17 22:40:06 +0200

    solve  addTwoDigits

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,14 @@
 export function addTwoDigits(n: any): number {
 
+    const numArr = n.toString().split("")
+    let sum : number = 0
+    for(const num of numArr){
+       sum += parseInt(num) 
+    }
+
+    return sum;
+
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
+console.log(addTwoDigits(84));

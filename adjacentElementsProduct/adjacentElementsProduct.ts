295c96a7de35f8017bf4c96261e07e048396e8d6
aboodswiti <aboodprinc@gmailcom>
2022-02-17 23:04:37 +0200

    solve adjacentElementsProduct

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,15 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
 
+    let max : number = null
+
+    for(let i =0 ; i<inputArray.length ; i++){
+        let result = inputArray[i] * inputArray[i+1]
+        if(result > max) max = result
+    }
+    return max
+
+
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([7, 6, -2, -5, 7, 7]));

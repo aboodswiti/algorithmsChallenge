89456e52fb755e7e62316146ba077fe8461d061e
aboodswiti <aboodprinc@gmailcom>
2022-02-17 23:22:21 +0200

    solve allLongestStrings

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,4 +1,11 @@
 export function allLongestStrings(inputArray: string[]): string[] {
+    let max = null
+    for (const str of inputArray) {
+        max = str.length > max ? str.length : max
+    }
+    const longestString = inputArray.filter((item) => item.length >= max)
+
+    return longestString
 
 }
 

7f1b736e068b9c5d1b794399886b20256f29bb5c
aboodswiti <swaiti393@gmail.com>
2022-02-18 20:48:40 +0200

    solve avoidObstacles

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,8 @@
 export function avoidObstacles(inputArray: number[]): number {
 
+    for (let i = 1; i < inputArray[inputArray.length - 1] + 1; i++) {
+        if (inputArray.every(item => item % i !== 0)) return i
+    }
 }
 
-// console.log(avoidObstacles([5, 3, 6, 7, 9]));
+console.log(avoidObstacles([5, 3, 6, 7, 9]));

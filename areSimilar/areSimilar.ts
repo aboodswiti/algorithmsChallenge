d33e0da34ca93e3ea86244d69e8a96d5da0907c3
aboodswiti <swaiti393@gmail.com>
2022-02-18 15:36:43 +0200

    solve export function areSimilar(a: number[], b: number[]): boolean {

--- areSimilar/areSimilar.ts
+++ areSimilar/areSimilar.ts
@@ -1,7 +1,26 @@
 export function areSimilar(a: number[], b: number[]): boolean {
+    const swapA: number[] = [];
+    let swapB: number[] = [];
+
+    if (a.toString() === b.toString()) return true;
+
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] !== b[i]) {
+            swapA.push(a[i]);
+            swapB.push(b[i]);
+        }
+    }
+    swapB.reverse();
+
+    if (swapA.length == 2 && (swapB.toString() === swapA.toString())) return true;
+
+    return false;
+
+
+
 
 }
 
-// console.log(areSimilar([1, 2, 3], [1, 2, 3]));
-// console.log(areSimilar([1, 2, 3], [2, 1, 3]));
-// console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+console.log(areSimilar([1, 2, 3], [1, 2, 3]));
+console.log(areSimilar([1, 2, 3], [2, 1, 3]));
+console.log(areSimilar([1, 2, 2], [2, 1, 1]));

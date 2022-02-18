2803d384fc6fe06ce7b0b1d6cc8b18aead9d5169
aboodswiti <swaiti393@gmail.com>
2022-02-18 18:34:46 +0200

    solve arrayMaximalAdjacentDifference

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,12 @@
 export function arrayMaximalAdjacentDifference(inputArray: number[]): number {
-
+    let max = 0;
+    let dif = 0;
+    
+    for(let i=0 ; i<inputArray.length -1; i++){
+      dif = Math.abs(inputArray[i] - inputArray[i+1])
+      if(max < dif) max = dif
+    }
+    return max;
 }
 
-// console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));

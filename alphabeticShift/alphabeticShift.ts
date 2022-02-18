359e5b8920f934c5a4a797ee9a2eaf0dcec2c59a
aboodswiti <swaiti393@gmail.com>
2022-02-18 02:12:20 +0200

    solve alphabeticShift

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,20 @@
 export function alphabeticShift(inputString: string): string {
+    const char = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+    const strArr = inputString.split("");
+    const shiftedArr = []
+    for (let i = 0; i < strArr.length; i++) {
+        let index = 0
+        if (strArr[i] !== "z") index = char.indexOf(strArr[i]) + 1;
+
+        shiftedArr.push(char[index])
+    }
+    return shiftedArr.join("")
+
+
+
 
 }
 
 console.log(alphabeticShift('crazy'));
+console.log(alphabeticShift('abcdefgh'));

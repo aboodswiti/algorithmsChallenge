35bb2e5f4da2ff8a483fb41d699a60288995f7b9
aboodswiti <swaiti393@gmail.com>
2022-02-18 03:22:30 +0200

    solve alternatingSums

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,29 @@
 export function alternatingSums(a: number[]): number[] {
+    
+    let oddSum = 0;
+    let evenSum = 0;
+
+    a.forEach((num, index) => {
+        if(index % 2 ===0) evenSum+= num
+        else oddSum+= num
+    })
+
+    return [evenSum,oddSum]
+
+
+    ////// SECOUND SOLUTION ///
+
+    // let firstTeam = 0;
+    // let secTeam = 0;
+    // for (let i = 0; i < a.length; i += 2) {
+    //     firstTeam += a[i]
+    //     if (i == a.length - 1) continue
+    //     secTeam += a[i + 1]
+
+    // }
+    // return [firstTeam, secTeam]
 
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums([20, 30, 20, 30, 20, 30, 20]))

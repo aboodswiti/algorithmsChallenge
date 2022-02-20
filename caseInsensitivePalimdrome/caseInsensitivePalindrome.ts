45c2e9c7578441ecd24e54542741468f99496609
aboodswiti <swaiti393@gmail.com>
2022-02-20 19:25:54 +0200

    solve isCaseInsensitivePalindrome

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,14 @@
 export function isCaseInsensitivePalindrome(inputString: string): boolean {
+    let strArr = inputString.toLocaleLowerCase()
+    let revStrArr = strArr.split("").reverse().join("")
+
+    return strArr === revStrArr
+  
+
+    
+
 
 }
 
-// console.log(isCaseInsensitivePalindrome('AaBaa'));
-// console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome('AaBaa'));
+console.log(isCaseInsensitivePalindrome('abac'));

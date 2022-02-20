7086514a7eacd4f0a3ff327cd225c1ad6029cd6c
aboodswiti <swaiti393@gmail.com>
2022-02-20 19:01:08 +0200

    solve candies

--- candies/candies.ts
+++ candies/candies.ts
@@ -1,5 +1,7 @@
 export function candies(n: number, m: number): number {
-
+    const num = Math.floor(m/n)
+    // return m - m % n 
+    return num * n
 }
 
-// console.log(candies(3, 10));
+console.log(candies(3, 10));

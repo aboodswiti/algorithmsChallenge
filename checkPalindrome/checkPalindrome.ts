6ef06b04b1b99193778abcf915787d53356cd03f
aboodswiti <swaiti393@gmail.com>
2022-03-16 19:25:18 +0200

    solve checkPalindrome

--- checkPalindrome/checkPalindrome.ts
+++ checkPalindrome/checkPalindrome.ts
@@ -1,7 +1,10 @@
 export function checkPalindrome(inputString: string): boolean {
+    const string = inputString.toLocaleLowerCase();
+    const reverseStr = string.split("").reverse().join("");
+    return reverseStr == string
 
 }
 
-// console.log(checkPalindrome('aabaa'));
-// console.log(checkPalindrome('abac'));
-// console.log(checkPalindrome('a'));
+console.log(checkPalindrome('aabaa'));
+console.log(checkPalindrome('abac'));
+console.log(checkPalindrome('a'));

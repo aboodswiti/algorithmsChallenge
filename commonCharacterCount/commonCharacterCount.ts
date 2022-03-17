44f56420b7835153d44c3092da05a3416c3357b2
aboodswiti <swaiti393@gmail.com>
2022-03-17 18:33:09 +0200

    solve commonCharacterCount

--- commonCharacterCount/commonCharacterCount.ts
+++ commonCharacterCount/commonCharacterCount.ts
@@ -1,5 +1,18 @@
 export function commonCharacterCount(s1: string, s2: string): number {
 
+    let arr1: string[] = s1.split("");
+    let arr2: string[] = s2.split("");
+    let count: number = 0;
+
+    for (let i = 0; i < arr1.length; i++) {
+        const index = arr2.indexOf(arr1[i])
+        if (index !== -1) {
+            count++
+            arr2.splice(index, 1)
+        }
+
+    }
+    return count;
 }
 
-// console.log(commonCharacterCount('aabcc', 'adcaa'));
+console.log(commonCharacterCount('aabcc', 'adcaa'));

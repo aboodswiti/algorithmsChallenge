d2fcc57ae9d97599be6c549cbbcd26784e5841d7
aboodswiti <swaiti393@gmail.com>
2022-03-17 17:43:39 +0200

    solve circleOfNumbers

--- circleOfNumbers/circleOfNumbers.ts
+++ circleOfNumbers/circleOfNumbers.ts
@@ -1,5 +1,8 @@
 export function circleOfNumbers(n: number, firstNumber: number): number {
-
+let half = n / 2;
+if(firstNumber < half) return firstNumber + half
+else return firstNumber - half
 }
 
-// console.log(circleOfNumbers(10, 2));
+console.log(circleOfNumbers(10, 2));
+console.log(circleOfNumbers(10, 7));

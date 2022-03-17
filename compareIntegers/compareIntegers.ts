58d456ac8bb240453932bd73d4e61e176ee7e6eb
aboodswiti <swaiti393@gmail.com>
2022-03-17 19:18:16 +0200

    solve compareIntegers

--- compareIntegers/compareIntegers.ts
+++ compareIntegers/compareIntegers.ts
@@ -1,7 +1,10 @@
 export function compareIntegers(a: string, b: string): string {
+const n1 = parseInt(a);
+const n2 = parseInt(b);
 
+return n1 > n2 ? "greater" : n1 < n2 ? "less" : "equal"
 }
 
-// console.log(compareIntegers('12', '13'));
-// console.log(compareIntegers('875', '799'));
-// console.log(compareIntegers('1000', '1000'));
+console.log(compareIntegers('12', '13'));
+console.log(compareIntegers('875', '799'));
+console.log(compareIntegers('1000', '1000'));

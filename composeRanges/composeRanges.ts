6677e437a82964dbc307d315b9c4f80cb77a4b47
aboodswiti <swaiti393@gmail.com>
2022-03-17 20:00:47 +0200

    solve composeRanges

--- composeRanges/composeRanges.ts
+++ composeRanges/composeRanges.ts
@@ -1,5 +1,17 @@
 export function composeRanges(nums: number[]): string[] {
+    const rangeArr = []
+    let start = 0
+    for (let i = 0; i < nums.length; i++) {
 
+        if (nums[i] - nums[i + 1] != -1 || i + 1 == nums.length) {
+            if (nums[start] == nums[i]) rangeArr.push(nums[start].toString())
+            else rangeArr.push(nums[start] + "->" + nums[i])
+            start = i + 1
+        }
+    }
+    return rangeArr
 }
 
-// console.log(composeRanges([-1, 0, 1, 2, 6, 7, 9]));
+console.log(composeRanges([-1, 0, 1, 2, 6, 7, 9]));
+console.log(composeRanges([-1, 0, 1, 2, 6, 7, 9]));
+console.log(composeRanges([0,3,7,8]));

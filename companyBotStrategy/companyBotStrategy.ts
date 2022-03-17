ae077f519f6256a90162799938579b331adb8bbe
aboodswiti <swaiti393@gmail.com>
2022-03-17 19:05:22 +0200

    solve companyBotStrategy

--- companyBotStrategy/companyBotStrategy.ts
+++ companyBotStrategy/companyBotStrategy.ts
@@ -1,7 +1,15 @@
 export function companyBotStrategy(trainingData: number[][]): number {
-
+    let total = 0
+    let count = 0
+    for(let i=0; i<trainingData.length;i++){
+        if(trainingData[i][1] == 1) {
+            total += trainingData[i][0]
+            count++
+        }
+    }
+    return total/count  || 0;
 }
 
-// console.log(companyBotStrategy([[3, 1], [6, 1], [4, 1], [5, 1]]));
-// console.log(companyBotStrategy([[4, 1], [4, -1], [0, 0], [6, 1]]));
-// console.log(companyBotStrategy( [[4, -1], [0, 0], [5, -1]]));
+console.log(companyBotStrategy([[3, 1], [6, 1], [4, 1], [5, 1]]));
+console.log(companyBotStrategy([[4, 1], [4, -1], [0, 0], [6, 1]]));
+console.log(companyBotStrategy([[4, -1], [0, 0], [5, -1]]));

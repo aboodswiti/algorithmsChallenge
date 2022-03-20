6b873b3bd28af9b7113be51d5ff9cc84092c95af
aboodswiti <swaiti393@gmail.com>
2022-03-20 19:01:38 +0200

    solve containsCloseNums

--- containsCloseNums/containsCloseNums.ts
+++ containsCloseNums/containsCloseNums.ts
@@ -1,6 +1,15 @@
 export function containsCloseNums(nums: number[], k: number): boolean {
 
+    let store = {}
+
+    for(let i=0; i< nums.length ; i++){
+        if(store[nums[i]]) {
+           if(Math.abs(store[nums[i]] - i ) <= k) return true;
+        } else store[nums[i]] = i
+    }
+    return false
+
 }
 
-// console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3));
-// console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 2));
+console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3));
+console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 2));

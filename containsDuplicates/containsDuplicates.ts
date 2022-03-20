868a1f3cbc704165bd60be6bcbc062953bef3bd2
aboodswiti <swaiti393@gmail.com>
2022-03-20 20:33:03 +0200

    solve containsDuplicates

--- containsDuplicates/containsDuplicates.ts
+++ containsDuplicates/containsDuplicates.ts
@@ -1,6 +1,13 @@
 export function containsDuplicates(a: number[]): boolean {
+    let store = {}
+    for (let i = 0; i < a.length; i++) {
 
+        if (store[a[i]]) return true
+        else store[a[i]] = a[i]
+
+    }
+    return false
 }
 
-// console.log(containsDuplicates([1, 2, 3, 1]));
-// console.log(containsDuplicates([3, 1]));
+console.log(containsDuplicates([1, 2, 3, 1]));
+console.log(containsDuplicates([3, 1]));

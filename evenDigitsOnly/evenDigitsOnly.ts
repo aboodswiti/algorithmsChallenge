d2e1a014d99b5ef866af9317b3654dcbfa355de4
aboodswiti <swaiti393@gmail.com>
2022-03-23 20:13:09 +0200

    solve evenGigitsOnly

--- evenDigitsOnly/evenDigitsOnly.ts
+++ evenDigitsOnly/evenDigitsOnly.ts
@@ -1,6 +1,9 @@
 export function evenDigitsOnly(n: number): boolean {
+    const num = n.toString().split("");
 
+    return num.every((item) => parseInt(item) % 2 == 0)
 }
 
-// console.log(evenDigitsOnly(248622));
-// console.log(evenDigitsOnly(642386));
+
+console.log(evenDigitsOnly(248622));
+console.log(evenDigitsOnly(642386));

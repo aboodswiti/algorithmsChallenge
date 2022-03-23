1fb4132e6f8f6a44c44a8296a1df31d14b7a712c
aboodswiti <swaiti393@gmail.com>
2022-03-23 16:51:06 +0200

    solve domainType

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,5 +1,15 @@
 export function domainType(domains: string[]): string[] {
+    const fullDomain = { org: "organization", com: "commercial", net: "network", info: "information"] }
+    const domainType: string[] = [];
 
+    for (let i = 0; i < domains.length; i++) {
+        const domain = domains[i].split(".")
+        const lastDomain = domain[domain.length - 1];
+        domainType.push(fullDomain[lastDomain])
+
+    }
+
+    return domainType
 }
 
-// console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
+console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));

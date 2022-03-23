bd163819d64454c53051fc7a731ce5afb6758471
aboodswiti <swaiti393@gmail.com>
2022-03-23 16:14:00 +0200

    solve differentSymbolsNaive

--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,5 +1,11 @@
 export function differentSymbolsNaive(s: string): number {
+    const store = {};
 
+
+    for (let i = 0; i < s.length; i++) {
+        store[s[i]] = 1
+    }
+    return Object.keys(store).length
 }
 
-// console.log(differentSymbolsNaive('cabca'));
+console.log(differentSymbolsNaive('cabca'));

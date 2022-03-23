1ff86d9be4709824a2c931dcef0f8ecf074a5bbb
aboodswiti <swaiti393@gmail.com>
2022-03-23 16:40:19 +0200

    solve digitDegree

--- digitDegree/digitDegree.ts
+++ digitDegree/digitDegree.ts
@@ -1,7 +1,15 @@
-export function digitDegree(n: number): number {
+export function digitDegree(n: any): number {
+    let count = 0;
 
+    while (true) {
+        if (n <= 9) break;
+        const num = n.toString().split('')
+        n = num.reduce((a, b) => parseInt(a) + parseInt(b))
+        count++
+    }
+    return count
 }
-
-// console.log(digitDegree(5));
-// console.log(digitDegree(10));
-// console.log(digitDegree(91));
+console.log(digitDegree(5));
+console.log(digitDegree(10));
+console.log(digitDegree(91));
+console.log(digitDegree(88));

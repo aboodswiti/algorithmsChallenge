4c5abf2ae661f5f3c0f1f6b34cca2ae1fe2eed55
aboodswiti <swaiti393@gmail.com>
2022-03-24 20:02:57 +0200

    solve extractMatrixColumn

--- extractMatrixColumn/extractMatrixColumn.ts
+++ extractMatrixColumn/extractMatrixColumn.ts
@@ -1,5 +1,7 @@
 export function extractMatrixColumn(matrix: number[][], column: number): number[] {
-
+    const res = [];
+    matrix.forEach((rows) => res.push(rows[column]))
+    return res;
 }
 
-// console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
+console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));

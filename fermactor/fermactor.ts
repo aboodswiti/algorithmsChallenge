3f28eacbd217056dcc08aec8c3ce86408f8334d3
aboodswiti <swaiti393@gmail.com>
2022-03-24 20:37:40 +0200

    solve fermactor

--- fermactor/fermactor.ts
+++ fermactor/fermactor.ts
@@ -1,5 +1,11 @@
 export function fermactor(n: number): number[] {
+    for (let i = 1; i < n; i++) {
+        for (let j = 1; j < i; j++) {
+            let res = i ** 2 - j ** 2
+            if (res == n) return [i, j]
+        }
 
+    }
 }
 
-// console.log(fermactor(15));
+console.log(fermactor(15));

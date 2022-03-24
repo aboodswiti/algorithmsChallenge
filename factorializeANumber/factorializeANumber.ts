9c9f8ca824de2b30eaf496defaeb662554a6b6a7
aboodswiti <swaiti393@gmail.com>
2022-03-24 20:04:40 +0200

    solve factorializeANumber

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,6 +1,14 @@
 export function factorializeANumber(num: number): number {
-
+    let factorial: number = 1;
+    if (num == 0) return 1;
+    else {
+        for (let i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+    }
+    return factorial;
 }
 
-// console.log(factorializeANumber(5));
-// console.log(factorializeANumber(10));
+console.log(factorializeANumber(5));
+console.log(factorializeANumber(10));
+console.log(factorializeANumber(0));
